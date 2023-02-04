b89f34304f8556e0edfefd1976e05686cd4d7ac2
Michael Pontus <m.pontus@gmail.com>
2023-02-04 23:25:33 +0300

    Add integ tests

---
+++ test/integ/integ.integ-inner-repro.ts
@@ -0,0 +1,18 @@
+import { ExpectedResult, IntegTest } from "@aws-cdk/integ-tests-alpha";
+import { App } from "aws-cdk-lib";
+import { IntegRunnerReproStack } from "../../lib/integ-runner-repro-stack";
+
+const app = new App();
+const stack = new IntegRunnerReproStack(app, "IntegRunnerReproStack");
+const integ = new IntegTest(app, "IntegRunnerReproIntegTest", {
+  testCases: [stack],
+});
+const invoke = integ.assertions.invokeFunction({
+  functionName: stack.handler.functionName,
+});
+invoke.expect(
+  ExpectedResult.objectLike({
+    StatusCode: 200,
+    Payload: "null",
+  })
+);
