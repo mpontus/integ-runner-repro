260b3c63d6fd1e99408cfd3f07c60f31c2ff2440
Michael Pontus <m.pontus@gmail.com>
2023-02-04 22:07:40 +0300

    Initial commit

---
+++ test/integ-runner-repro.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IntegRunnerRepro from '../lib/integ-runner-repro-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/integ-runner-repro-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IntegRunnerRepro.IntegRunnerReproStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
