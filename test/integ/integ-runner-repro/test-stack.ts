b92ad65b7c699b5da173469502143774a998e44d
Michael Pontus <m.pontus@gmail.com>
2023-02-05 18:30:44 +0300

    Separate test stack

---
+++ test/integ/integ-runner-repro/test-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+
+export class IntegRunnerReproTestStack extends cdk.Stack {
+  public readonly handler: lambda.Function;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.handler = new NodejsFunction(this, "Handler", {
+      entry: `${__dirname}/handlers.ts`,
+      handler: "handler",
+    });
+  }
+}
