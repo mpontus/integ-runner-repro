9a98c68cb43ec6d2e0b50a047412a815ae5ed309
Michael Pontus <m.pontus@gmail.com>
2023-02-05 15:31:52 +0300

    Extract test stack

--- test/integ/integ.integ-inner-repro.ts
+++ test/integ/integ.integ-inner-repro.ts
@@ -1,18 +1,14 @@
 import { ExpectedResult, IntegTest } from "@aws-cdk/integ-tests-alpha";
-import { App, Stack } from "aws-cdk-lib";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { App } from "aws-cdk-lib";
+import { IntegRunnerReproTestStack } from "./integ-runner-repro/test-stack";
 
 const app = new App();
-const stack = new Stack(app, "IntegRunnerReproStack");
-const handler = new NodejsFunction(stack, "RunAllTests", {
-  entry: `${__dirname}/handlers.ts`,
-  handler: "handler",
-});
+const stack = new IntegRunnerReproTestStack(app, "IntegRunnerReproStack");
 const integ = new IntegTest(app, "IntegRunnerReproIntegTest", {
   testCases: [stack],
 });
 const invoke = integ.assertions.invokeFunction({
-  functionName: handler.functionName,
+  functionName: stack.handler.functionName,
 });
 invoke.expect(
   ExpectedResult.objectLike({
